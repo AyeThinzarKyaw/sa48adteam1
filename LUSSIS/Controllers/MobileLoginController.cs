173afe65728527ac5ef2e193ea220550b23161ea
Charles Wen <charleswen88@gmail.com>
2019-07-30 13:37:24 +0800

    Add working mobile login api

---
+++ LUSSIS/Controllers/MobileLoginController.cs
@@ -0,0 +1,34 @@
+﻿using LUSSIS.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LUSSIS.Controllers
+{
+    public class MobileLoginController : ApiController
+    {
+        private LUSSISContext db = new LUSSISContext();
+
+        // POST: api/Login
+        public string Post([FromBody]Employee employee)
+        {
+            if(db.Employees.Any(user => user.Username.Equals(employee.Username)))
+            {
+                Employee emp = db.Employees.Where(user => user.Username.Equals(employee.Username)).First();
+                if(employee.Password.Equals(emp.Password))
+                    return JsonConvert.SerializeObject(emp);
+                else
+                    return JsonConvert.SerializeObject("Wrong Password");
+            }
+            else
+            {
+                return JsonConvert.SerializeObject("Username does not exist");
+            }
+
+        }
+    }
+}
