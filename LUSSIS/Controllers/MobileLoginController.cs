78785b94b126b9866603c7616132ad71a2ed00bd
Charles Wen <charleswen88@gmail.com>
2019-07-30 18:09:28 +0800

    Update MobileLoginController

--- LUSSIS/Controllers/MobileLoginController.cs
+++ LUSSIS/Controllers/MobileLoginController.cs
@@ -13,20 +13,21 @@ namespace LUSSIS.Controllers
     {
         private LUSSISContext db = new LUSSISContext();
 
-        // POST: api/Login
-        public string Post([FromBody]Employee employee)
+        //POST: api/MobileLogin
+        //Written by Charles
+        public Employee Post([FromBody]Employee employee)
         {
             if(db.Employees.Any(user => user.Username.Equals(employee.Username)))
             {
                 Employee emp = db.Employees.Where(user => user.Username.Equals(employee.Username)).First();
                 if(employee.Password.Equals(emp.Password))
-                    return JsonConvert.SerializeObject(emp);
+                    return emp;
                 else
-                    return JsonConvert.SerializeObject("Wrong Password");
+                    return null;
             }
             else
             {
-                return JsonConvert.SerializeObject("Username does not exist");
+                return null;
             }
 
         }
