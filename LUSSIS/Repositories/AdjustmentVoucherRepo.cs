51b1593a830ede5f69e9beb15412b8a49f2f4c32
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-08-01 15:07:12 +0800

    StationeryId from wrongspelling

--- LUSSIS/Repositories/AdjustmentVoucherRepo.cs
+++ LUSSIS/Repositories/AdjustmentVoucherRepo.cs
@@ -22,7 +22,7 @@ namespace LUSSIS.Repositories
                 IQueryable query = from av in Context.AdjustmentVouchers
                                    join avd in Context.AdjustmentVoucherDetails on av.Id equals avd.AdjustmentVoucherId
                                    where av.Status.Equals("Open") || av.Status.Equals("Pending")
-                                   where avd.StationeyId == stationeryId
+                                   where avd.StationeryId == stationeryId
                                    select avd.Quantity;
                 if (query != null)
                 {
