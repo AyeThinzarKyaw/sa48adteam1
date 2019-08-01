ad118d2d4e27772ef213393db75257c1f6751908
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-08-01 13:44:03 +0800

    add new files(controller, iservice, service, catalogueDto for PO

---
+++ LUSSIS/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LUSSIS.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        // GET: PurchaseOrder
+        //By ATZK
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ LUSSIS/Services/PurchaseOrderService.cs
@@ -0,0 +1,31 @@
+﻿using LUSSIS.Models;
+using LUSSIS.Repositories;
+using LUSSIS.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSIS.Services
+{
+    public class PurchaseOrderService :IPurchaseOrderService
+    {
+        private PurchaseOrderService() { }
+
+        private static PurchaseOrderService instance = new PurchaseOrderService();
+        public static IPurchaseOrderService Instance
+        {
+            get { return instance; }
+        }
+
+        public IEnumerable<PurchaseOrder> getAllPurchaseOrders()
+        {
+            return PurchaseOrderRepo.Instance.FindAll().OrderByDescending(x=>x.OrderDateTime).ToList();
+        }
+
+        public void RetrievePurchaseOrderCatalogue()
+        {
+
+        }
+    }
+}
