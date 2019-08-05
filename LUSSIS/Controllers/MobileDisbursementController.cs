ce385ad54263f2468f5513c8de956046fa49ddcc
Charles Wen <charleswen88@gmail.com>
2019-08-05 20:02:34 +0800

    Added MobileDisbursementController

---
+++ LUSSIS/Controllers/MobileDisbursementController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LUSSIS.Controllers
+{
+    public class MobileDisbursementController : ApiController
+    {
+
+        // GET: api/MobileDisbursement/5
+        public string Get(int id)
+        {
+            //Get all disbursements from department using EmployeeId
+            return "value";
+        }
+
+    }
+}
