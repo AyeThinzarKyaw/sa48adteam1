565e7940dfccdfaf026c0b0636f05933098b7dfb
Wang Shuo <wangshuoliang@hotmail.com>
2019-08-10 00:51:27 +0800

    Disbursement Use case: controller, DTO, repo and Irepo, serv and Iserve, two views

---
+++ LUSSIS/Services/Interfaces/IDisbursementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LUSSIS.Models;
+using LUSSIS.Models.DTOs;
+
+namespace LUSSIS.Services.Interfaces
+{
+    public interface IDisbursementService
+    {
+        List<DisbursementListDTO> GetDepRepDisbursementsDetails(int EmployeeId);
+        List<DisbursementListDTO> GetClerkDisbursementsDetails(int EmployeeId);
+    }
+}
