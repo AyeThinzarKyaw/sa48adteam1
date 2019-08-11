cc0e67c27cee5620a35387d5b11389a3336acf0d
Charles Wen <charleswen88@gmail.com>
2019-08-11 19:48:19 +0800

    Added Mobile Retrieval

--- LUSSIS/App_Start/WebApiConfig.cs
+++ LUSSIS/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace LUSSIS
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
