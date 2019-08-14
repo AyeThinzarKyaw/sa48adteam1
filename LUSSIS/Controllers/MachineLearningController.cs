1cbcdc626d0dfccdbcde271b721e9cb2cc8751da
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-08-14 17:26:26 +0800

    update ML controller

--- LUSSIS/Controllers/MachineLearningController.cs
+++ LUSSIS/Controllers/MachineLearningController.cs
@@ -19,11 +19,11 @@ namespace LUSSIS.Controllers
         // GET: MachineLearning
         public ActionResult MachineLearning()
         {
-            return View();
+            return View(new MachineLearningDTO() { chosenDate=DateTime.Now});
         }
 
         [HttpPost]
-        public async Task<ActionResult> MachineLearning(MachineLearningDTO predModel)
+        public async Task<ActionResult> MachineLearning(MachineLearningDTO predictedDate)
         {
             using (var client = new HttpClient())
             {
@@ -32,84 +32,59 @@ namespace LUSSIS.Controllers
 
                 // send a POST request to the server uri with the data and get the response as HttpResponseMessage object
                 // add 'Microsoft.AspNet.WebApi.Client' Nuget package
-                HttpResponseMessage res = await client.PostAsJsonAsync("http://127.0.0.1:5000/", predModel);
+                //HttpResponseMessage res = await client.PostAsJsonAsync("http://127.0.0.1:5000/", predModel);
 
                 // Return the result from the server if the status code is 200 (everything is OK)
                 // should raise exception or error if it's not
-                if (res.IsSuccessStatusCode)
-                {
+                //if (res.IsSuccessStatusCode)
+                //{
                     // pass the result by setting the Viewdata property
                     // have to read as string for the data in response.body
 
                     List<Stationery> stationeries = StationeryService.Instance.GetAllStationeries().ToList();
                     List<Stationery> updatedStationeries = new List<Stationery>();
-
+                    updatedStationeries = stationeries;
                    
-                    JArray jsonArray = JArray.Parse(res.Content.ReadAsStringAsync().Result);
-                    int i;
-                    foreach (JArray ja in jsonArray)
-                    {
-                        i = 0;
-                        int currentId = 0;
-                        foreach (string a in ja)
-                        {
-                            if (i == 0)
-                            {
-                                currentId = Convert.ToInt32(a);
-                                updatedStationeries.Add(stationeries.Find(x => x.Id == currentId));
-                                
-                            }
-                                
-                            if (i == 1)
-                            {
-                                int qty= Convert.ToInt32(a);
-                                updatedStationeries.Find(x => x.Id == currentId).ReorderLevel = qty;
-                                updatedStationeries.Find(x => x.Id == currentId).ReorderQuantity = qty;
-                            }
-                           
-                            i = i + 1;
-                        }
-                        
-                    }
-                    //string[] ElementArray = new string[6];
-                    //ArrayList arrayList1 = new ArrayList();
                     //JArray jsonArray = JArray.Parse(res.Content.ReadAsStringAsync().Result);
                     //int i;
                     //foreach (JArray ja in jsonArray)
                     //{
                     //    i = 0;
+                    //    int currentId = 0;
                     //    foreach (string a in ja)
                     //    {
                     //        if (i == 0)
-                    //            ElementArray[0] = a;
+                    //        {
+                    //            currentId = Convert.ToInt32(a);
+                    //            updatedStationeries.Add(stationeries.Find(x => x.Id == currentId));
+                                
+                    //        }
+                                
                     //        if (i == 1)
-                    //            ElementArray[1] = a;
-                    //        //if (i == 2)
-                    //        //    ElementArray[2] = a;
-                    //        //if (i == 3)
-                    //        //    ElementArray[3] = a;
-                    //        //if (i == 4)
-                    //        //    ElementArray[4] = a;
-                    //        //if (i == 5)
-                    //        //    ElementArray[5] = a;
+                    //        {
+                    //            int qty= Convert.ToInt32(a);
+                    //            updatedStationeries.Find(x => x.Id == currentId).ReorderLevel = qty;
+                    //            updatedStationeries.Find(x => x.Id == currentId).ReorderQuantity = qty;
+                    //        }
+                           
                     //        i = i + 1;
                     //    }
-                    //    arrayList1.Add(ja);
+                        
                     //}
                     TempData["updatedStationeryQty"] = updatedStationeries;
                     ViewBag.data = updatedStationeries;
-                    return View();
-                }
-                else
-                {
-                    return View("Error");
-                }
+                    return View(predictedDate);
+                //}
+                //else
+                //{
+                //    return View("Error");
+                //}
 
             }
 
         }
 
-        public void UpdateReorderQuantity()
+        public ActionResult UpdateReorderQuantity(DateTime predictDate)
         {
             List<Stationery> stationeries = (List<Stationery>)TempData["updatedStationeryQty"];
             TempData.Keep("updatedStationeryQty");
@@ -120,7 +95,9 @@ namespace LUSSIS.Controllers
                     StationeryService.Instance.UpdateStationery(item);
                 }
             }
-
+            ViewBag.data = stationeries;
+            ViewBag.Message = "Successfully updated the reorder information.";
+            return View("MachineLearning", new MachineLearningDTO() { chosenDate = predictDate });
         }
     }
 }
--- LUSSIS/Models/DTOs/MachineLearningDTO.cs
+++ LUSSIS/Models/DTOs/MachineLearningDTO.cs
@@ -7,9 +7,11 @@ namespace LUSSIS.Models.DTOs
 {
     public class MachineLearningDTO
     {
-        public int InputYear { get; set; }
-        public int InputMonth { get; set; }
-        public int InputDay { get; set; }
+        //public int InputYear { get; set; }
+        //public int InputMonth { get; set; }
+        //public int InputDay { get; set; }
+
+        public DateTime chosenDate { get; set; }
 
     }
 }
