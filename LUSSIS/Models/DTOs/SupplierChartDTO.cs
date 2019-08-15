a8085cbc0134b55b194e906deba540685c0f9b6a
Wang Shuo <wangshuoliang@hotmail.com>
2019-08-15 18:10:29 +0800

    Chart Partially done

---
+++ LUSSIS/Models/DTOs/SupplierChartDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSIS.Models.DTOs
+{
+    public class SupplierChartDTO
+    {
+        public List<SupplierChartDTO> SupplierChartDTOs { get; set; }
+        public int Id { get; set; }
+        public int PurchaseOrderId { get; set; }
+        public int StationeryId { get; set; }
+        public int QuantityOrdered { get; set; }
+        public int QuantityDelivered { get; set; }
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public string SupplierName { get; set; }
+        public decimal ItemUnitPrice { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+
+        public PurchaseOrderDetail PurchaseOrderDetailForChart { get; set; }
+        public PurchaseOrder PurchaseOrderForChart { get; set; }
+        public SupplierTender SupplierTenderForChart { get; set; }
+        public Employee EmployeeForChart { get; set; }
+        public Supplier SupplierForChart { get; set; }
+        public Stationery StationeryForChart { get; set; }
+        public Category CategoryForChart { get; set; }
+
+    }
+}
--- LUSSIS/Repositories/PurchaseOrderDetailRepo.cs
+++ LUSSIS/Repositories/PurchaseOrderDetailRepo.cs
@@ -18,5 +18,18 @@ namespace LUSSIS.Repositories
             get { return instance; }
         }
 
+        public List<PurchaseOrderDetail> GetPurchaseOrderDetailsBySupplierId(int SupplierId)
+        {
+            var results = from pod in Context.PurchaseOrderDetails
+                          join po in Context.PurchaseOrders on pod.PurchaseOrderId equals po.Id
+                          join e in Context.Employees on po.EmployeeId equals e.Id
+                          join su in Context.Suppliers on po.SupplierId equals su.Id
+                          join s in Context.Stationeries on pod.StationeryId equals s.Id
+                          join c in Context.Categories on s.CategoryId equals c.Id
+
+                          where su.Id == SupplierId
+                          select pod;
+            return results.ToList();
+        }
     }
 }
